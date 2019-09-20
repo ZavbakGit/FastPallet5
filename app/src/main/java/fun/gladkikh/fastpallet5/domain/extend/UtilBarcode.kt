12cf9ee21e204c905ce28e5c3edfdab2b26665df
ZavbakGit <ag@799000.ru>
2019-09-20 17:14:32 +1000

    yes

--- app/src/main/java/fun/gladkikh/fastpallet5/domain/extend/UtilBarcode.kt
+++ app/src/main/java/fun/gladkikh/fastpallet5/domain/extend/UtilBarcode.kt
@@ -2,6 +2,8 @@ package `fun`.gladkikh.fastpallet5.domain.extend
 
 import `fun`.gladkikh.fastpallet5.domain.intety.Box
 import `fun`.gladkikh.fastpallet5.domain.intety.Product
+import io.reactivex.Flowable
+import java.math.BigDecimal
 
 fun Product.getWeightFromBarcode(barcode: String): Float {
     return barcode.substring(0..2).toFloatOrNull() ?: 0f
@@ -23,3 +25,130 @@ fun List<Box>.getInfoWrap(): InfoListBoxWrap {
 }
 
 data class InfoListBoxWrap(var countBox: Int, var weight: Float)
+
+fun getWeightByBarcode(barcode: String, start: Int, finish: Int, coff: Float): Float {
+
+    if (start == 0) {
+        return 0F
+    }
+
+    if (finish == 0) {
+        return 0F
+    }
+
+    if (barcode.length == 0) return 0F
+    if (start >= finish) return 0F
+    if (barcode.length < finish) return 0F
+
+    var weightInt: Int
+
+    try {
+        weightInt = barcode.subSequence(start - 1, finish).toString().toIntOrNull() ?: 0
+    } catch (e: Exception) {
+        weightInt = 0
+    }
+
+    return BigDecimal(weightInt)
+        .multiply(BigDecimal(coff.toString())).toString().toFloatOrNull() ?: 0f
+
+}
+
+fun isPallet(barcode: String): Boolean {
+    return barcode.startsWith("<pal>", true) && barcode.endsWith("</pal>", true)
+}
+
+fun getNumberDocByBarCode(barcode: String): String {
+    //<pal>021400000007</pal>
+    if (!isPallet(barcode)) {
+        throw Throwable("Не паллета!")
+    }
+
+    var strCode = barcode.replace("<pal>", "").replace("</pal>", "")
+
+
+    var finishPref = 2 + strCode.substring(0, 2).toInt()
+    var strKir = strCode.substring(2, finishPref)
+
+    var prefKir = Flowable.fromIterable(strKir.toCharArray().toList())
+        .buffer(2)
+        .map {
+            getCyrillicLetterByNumber(it.joinToString(""))
+        }
+        .toList().blockingGet().joinToString("")
+
+
+    return prefKir + strCode.takeLast(strCode.length - finishPref)
+}
+
+fun getCyrillicLetterByNumber(s: String): String {
+
+    return when (s) {
+        "01" -> "А"
+
+        "02" -> "Б"
+
+        "03" -> "В"
+
+        "04" -> "Г"
+
+        "05" -> "Д"
+
+        "06" -> "Е"
+
+        "07" -> "Ё"
+
+        "08" -> "Ж"
+
+        "09" -> "З"
+
+        "10" -> "И"
+
+        "11" -> "Й"
+
+        "12" -> "К"
+
+        "13" -> "Л"
+
+        "14" -> "М"
+
+        "15" -> "Н"
+
+        "16" -> "О"
+
+        "17" -> "П"
+
+        "18" -> "Р"
+
+        "19" -> "С"
+
+        "20" -> "Т"
+
+        "21" -> "У"
+
+        "22" -> "Ф"
+
+        "23" -> "Х"
+
+        "24" -> "Ц"
+
+        "25" -> "Ч"
+
+        "26" -> "Ш"
+
+        "27" -> "Щ"
+
+        "28" -> "Ъ"
+
+        "29" -> "Ы"
+
+        "30" -> "Ь"
+
+        "31" -> "Э"
+
+        "32" -> "Ю"
+
+        "33" -> "Я"
+
+        else -> ""
+    }
+}
--- app/src/main/java/fun/gladkikh/fastpallet5/viewmodel/creatpallet/ProductCreatPalletViewModel.kt
+++ app/src/main/java/fun/gladkikh/fastpallet5/viewmodel/creatpallet/ProductCreatPalletViewModel.kt
@@ -1,9 +1,12 @@
 package `fun`.gladkikh.fastpallet5.viewmodel.creatpallet
 
+import `fun`.gladkikh.fastpallet5.domain.extend.isPallet
 import `fun`.gladkikh.fastpallet5.domain.intety.CreatePallet
 import `fun`.gladkikh.fastpallet5.domain.intety.Pallet
 import `fun`.gladkikh.fastpallet5.domain.intety.Product
+import `fun`.gladkikh.fastpallet5.domain.intety.Status
 import `fun`.gladkikh.fastpallet5.repository.CreatePalletRepository
+import `fun`.gladkikh.fastpallet5.viewmodel.BaseViewModelFragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -12,13 +15,28 @@ import java.util.*
 class ProductCreatPalletViewModel(
     val guidDoc: String,
     val guidProduct: String
-) : ViewModel() {
+) : BaseViewModelFragment() {
+
+
+    var docCreatePallet:CreatePallet? = null
+        set(value) {
+            field = value
+        }
+
+
+    lateinit var status:Status
 
     fun getDocLiveData(): LiveData<CreatePallet> = CreatePalletRepository.getDocByGuid(guidDoc)
     fun getProductLiveData(): LiveData<Product> = CreatePalletRepository.getProductByGuid(guidProduct)
     fun getListPalletByProduct() = CreatePalletRepository.getListPalletByProduct(guidProduct)
 
     fun addPallet(barcode: String) {
+        when{
+            isPallet(barcode) -> messageError.postValue("Это не штрих код паллеты!")
+            docCreatePallet.status?.let { Status.getStatusById(it) }
+                    in [null,Status.LOADED,Status.UNLOADED] ->  messageError.postValue("Это не штрих код паллеты!")
+        }
+
         //ToDo Выполнить необходимые проверки при добавлении паллеты
         val pallet = Pallet(
             guid = UUID.randomUUID().toString(),
@@ -35,6 +53,10 @@ class ProductCreatPalletViewModel(
         CreatePalletRepository.savePallet(pallet, guidProduct)
     }
 
+    fun setDoc(doc:CreatePallet){
+        docCreatePallet = doc
+    }
+
     class ViewModelFactory(
         private val guid: String,
         private val guidStringProduct: String
@@ -44,5 +66,4 @@ class ProductCreatPalletViewModel(
             return ProductCreatPalletViewModel(guid, guidStringProduct) as T
         }
     }
-
 }
