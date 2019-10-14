e2822851b31082aee070a5ccce2608f9822a25fe
ZavbakGit <ag@799000.ru>
2019-10-14 19:12:40 +1000

    test

---
+++ app/src/androidTest/java/fun/gladkikh/fastpallet5/TestAppJUnitRunner.kt
@@ -0,0 +1,12 @@
+package `fun`.gladkikh.fastpallet5
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+
+class TestAppJUnitRunner : AndroidJUnitRunner() {
+
+    override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
+        return super.newApplication(cl, TestApp::class.java.name, context)
+    }
+}
--- app/src/test/java/fun/gladkikh/fastpallet5/ui/CreatePalletViewModelTest.kt
+++ app/src/test/java/fun/gladkikh/fastpallet5/ui/CreatePalletViewModelTest.kt
@@ -5,17 +5,13 @@ import `fun`.gladkikh.fastpallet5.domain.intety.Product
 import `fun`.gladkikh.fastpallet5.domain.intety.Type
 import `fun`.gladkikh.fastpallet5.repository.CreatePalletRepository
 import `fun`.gladkikh.fastpallet5.ui.fragment.creatpallet.doc.CreatePalletViewModel
-
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.reset
-import com.nhaarman.mockitokotlin2.times
-import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import junit.framework.Assert
-import junit.framework.Assert.assertEquals
-import junit.framework.Assert.assertFalse
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -29,13 +25,15 @@ class CreatePalletViewModelTest {
     private val mockCreatePalletRepository = mock<CreatePalletRepository>()
     private lateinit var viewModel: CreatePalletViewModel
 
+    private val testFoo = mock<Foo>()
+
     private val creatPalletLiveData = MutableLiveData<CreatePallet>()
     private val listProductLiveData = MutableLiveData<List<Product>>()
 
     val testDoc = CreatePallet(
         guid = "12345",
         barcode = "46546546546",
-        dataChanged =  Date(),
+        dataChanged = Date(),
         date = Date(),
         description = "Описание с сервера",
         guidServer = "54321",
@@ -43,24 +41,32 @@ class CreatePalletViewModelTest {
         number = "5",
         status = 1,
         typeFromServer = Type.CREATE_PALLET.nameServer,
-        listProduct = listOf(Product(guid = "123"),Product(guid = "321"))
-
+        listProduct = listOf(Product(guid = "123"), Product(guid = "321"))
     )
 
     @Before
-    fun setup(){
+    fun setup() {
         reset(mockCreatePalletRepository)
-        whenever(mockCreatePalletRepository.getDocByGuidLd(testDoc.guid)).thenReturn(creatPalletLiveData)
-        whenever(mockCreatePalletRepository.getListProductByDocLd(testDoc.guid))
-            .thenReturn(listProductLiveData)
-
-
+        whenever(mockCreatePalletRepository.getDocByGuidLd(testDoc.guid)).thenReturn(
+            creatPalletLiveData
+        )
+        whenever(mockCreatePalletRepository.getListProductByDocLd(testDoc.guid)).thenReturn(
+            listProductLiveData
+        )
         viewModel = CreatePalletViewModel(mockCreatePalletRepository)
+
+        //А здесь при любой строке
+        whenever(testFoo.getList(any())).thenReturn((0..100).map {
+            it.toString()
+        })
+
+        //Обращение к переменной
+        whenever(testFoo.str).thenReturn("Hi")
     }
 
     @Test
     fun `setCreatePallet should return CreatePallet`() {
-        var result:CreatePallet? =  null
+        var result: CreatePallet? = null
         var testData = testDoc
 
 
@@ -71,9 +77,21 @@ class CreatePalletViewModelTest {
         creatPalletLiveData.value = testDoc
         listProductLiveData.value = testData.listProduct
 
+        println(testFoo.str)
+        println(testFoo.getList("kkk").joinToString (";"))
+
+
         Assert.assertEquals(testData, result)
     }
 
+}
 
+class Foo {
+    val str = "456"
 
+    fun getList(str: String): List<String> {
+        return (0..10).map {
+            it.toString() + str
+        }
+    }
 }
